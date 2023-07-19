36c7dd099fff9ba342e0651090672e0d98f2d7fb
Lyas <Egorlyasotsky@yandex.ru>
2023-07-19 18:37:22 +0400

    Program is ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InputArray = new string[5] {"123", "23", "hello", "world", "res"};
+string[] ResultArray = new string[InputArray.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(InputArray, ResultArray);
+PrintArray(ResultArray);
